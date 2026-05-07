f0199843c66dc5a8d3125ad7aad58e54b98421a3
jogaudard <joseph.gaudard@pm.me>
2026-05-07 17:21:11 +0200

    some cleaning

--- R/incline_plan.R
+++ R/incline_plan.R
@@ -1,14 +1,4 @@
 incline_plan <- list(
-#   tar_target(
-#     name = import_fluxes_incline_2020,
-#     command = get_file(
-#       node = "zhk3m",
-#       file = "INCLINE_c-flux_2020.csv",
-#       path = "data",
-#       remote_path = "C-Flux"
-#     ),
-#     format = "file"
-#   ),
   tar_target(
     name = import_fluxes_incline_2022,
     command = get_file(
@@ -29,37 +19,14 @@ incline_plan <- list(
     ),
     format = "file"
     ),
-    # tar_target(
-    #     name = raw_incline_metadata,
-    #     command = "data/INCLINE_metadata.csv",
-    #     format = "file"
-    # ),
     tar_target(
         name = read_incline_metadata,
         command = read_csv2(import_incline_metadata, show_col_types = FALSE)
     ),
-    # tar_target(
-    #     name = raw_fluxes_incline_2020,
-    #     command = "data/INCLINE_c-flux_2020.csv",
-    #     format = "file"
-    # ),
-    # tar_target(
-    #     name = raw_fluxes_incline_2022,
-    #     command = "data/INCLINE_c-flux_2022.csv",
-    #     format = "file"
-    # ),
-    # tar_target(
-    #     name = read_fluxes_incline_2020,
-    #     command = read_csv(raw_fluxes_incline_2020, show_col_types = FALSE)
-    # ),
     tar_target(
         name = read_fluxes_incline_2022,
         command = read_csv(import_fluxes_incline_2022, show_col_types = FALSE)
     ),
-    # tar_target(
-    #     name = read_fluxes_incline,
-    #     command = bind_rows(read_fluxes_incline_2020, read_fluxes_incline_2022)
-    # ),
     tar_target(
         name = prepare_fluxes_incline,
         command = prepare_fluxes_incline_fun(read_fluxes_incline_2022, read_incline_metadata)
