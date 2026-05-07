73bfecac11430636dc01960774ceefef4477ebb2
jogaudard <joseph.gaudard@pm.me>
2026-05-07 14:07:18 +0200

    needed pivot wider

--- R/functions/incline_fun.R
+++ R/functions/incline_fun.R
@@ -5,6 +5,7 @@ prepare_fluxes_incline_fun <- function(read_fluxes_incline_2022, read_incline_me
       filter(
         treatment == "C"
         & type %in% c("NEE", "GPP")
+        & is.na(par_correction)
       ) |>
       mutate(
     date = date(f_datetime),
@@ -17,7 +18,7 @@ prepare_fluxes_incline_fun <- function(read_fluxes_incline_2022, read_incline_me
     ),
     f_flux = f_flux * 0.04401 # they want g CO2 / m2 / h
   ) |>
-  select(plotID, siteID, date, OTC, temp_soil_ave, f_flux, f_temp_air_ave, ITEX_ID,  f_RMSE, PAR_ave) |>
+  select(plotID, siteID, date, OTC, temp_soil_ave, f_flux, f_temp_air_ave, ITEX_ID,  f_RMSE, PAR_ave, type) |>
   rename(
     treatment = "OTC",
     RMSE = "f_RMSE",
@@ -26,6 +27,7 @@ prepare_fluxes_incline_fun <- function(read_fluxes_incline_2022, read_incline_me
     temp_soilavg = "temp_soil_ave"
   ) |>
   drop_na(date, flux) |>
+  pivot_wider(names_from = "type", values_from = "flux") |>
   arrange(ITEX_ID) |>
   relocate(ITEX_ID, date, treatment, plotID, flux, RMSE, temp_airavg, temp_soilavg, siteID)
 }
